9cc99b4c679c03e84ff6b40bc889d34c8707c0da
omairapalacios <cpalaciosanchez@hotmail.com>
2022-01-22 01:14:20 -0500

    fix url base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://omairapalacios.github.io/',
+  base: 'https://omairapalacios.github.io/social-react/',
   plugins: [react()]
 })
